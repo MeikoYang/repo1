ef1e7570c534c29b4a9f6e687d4b6396fcb1a709
MeikoYang <474792194@qq.com>
2019-02-21 19:49:41 +0800

    把本地工程添加到git

---
+++ git_test_project/src/main/java/com/itheima/git/FirstClass.java
@@ -0,0 +1,16 @@
+package com.itheima.git;
+
+/**
+ * @Author:MeikoYang
+ * @Date:2019/2/21 0021
+ * @Description:
+ */
+
+public class FirstClass {
+    public static void main(String[] args) {
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+        System.out.println("hello git project");
+    }
+}
